53af87a734c057b7cd38055426942b8b3bdbe2bb
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-09-23 21:54:12 +0900

    ABC 156 B - Digits

--- a.cpp
+++ a.cpp
@@ -1,95 +1,90 @@
 #include <bits/stdc++.h>
 //#include <atcoder/all>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
-//using namespace atcoder;
+// using namespace atcoder;
 using namespace std;
 using ll = long long;
 using P = pair<ll, ll>;
 //マクロ
-//forループ関係
+// forループ関係
 //引数は、(ループ内変数,動く範囲)か(ループ内変数,始めの数,終わりの数)、のどちらか
-//Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
+// Dがついてないものはループ変数は1ずつインクリメントされ、Dがついてるものはループ変数は1ずつデクリメントされる
 #define FOR(i, a, b) for (ll i = a; i <= (ll)(b); i++)
-//xにはvectorなどのコンテナ
-#define ALL(x) (x).begin(), (x).end() //sortなどの引数を省略したい
-#define SIZE(x) ((ll)(x).size())      //sizeをsize_tからllに直しておく
-#define MAX(x) *max_element(ALL(x))   //最大値を求める
-#define MIN(x) *min_element(ALL(x))   //最小値を求める
+// xにはvectorなどのコンテナ
+#define ALL(x) (x).begin(), (x).end()  // sortなどの引数を省略したい
+#define SIZE(x) ((ll)(x).size())       // sizeをsize_tからllに直しておく
+#define MAX(x) *max_element(ALL(x))    //最大値を求める
+#define MIN(x) *min_element(ALL(x))    //最小値を求める
 //定数
-#define INF 1000000000000 //10^12:極めて大きい値,∞
-#define inf 2147483647    //int値の最大値
-#define MOD 1000000007    //10^9+7:合同式の法
-#define MAXR 100000       //10^5:配列の最大のrange(素数列挙などで使用)
+#define INF 1000000000000  // 10^12:極めて大きい値,∞
+#define inf 2147483647     // int値の最大値
+#define MOD 1000000007     // 10^9+7:合同式の法
+#define MAXR 100000        // 10^5:配列の最大のrange(素数列挙などで使用)
 //略記
-#define PB push_back                            //vectorヘの挿入
-#define MP make_pair                            //pairのコンストラクタ
-#define F first                                 //pairの一つ目の要素
-#define S second                                //pairの二つ目の要素
-#define CST(x) cout << fixed << setprecision(x) //小数点以下の桁数指定
-template <class T>
-inline bool chmin(T &a, T b){
-    if (a > b){
-        a = b;
-        return true;        
-    }
-    return false;
-}
-template <class T>
-inline bool chmax(T &a, T b){
-    if (a < b)
-    {
+#define PB push_back                             // vectorヘの挿入
+#define MP make_pair                             // pairのコンストラクタ
+#define F first                                  // pairの一つ目の要素
+#define S second                                 // pairの二つ目の要素
+#define CST(x) cout << fixed << setprecision(x)  //小数点以下の桁数指定
+template <class T> inline bool chmin(T &a, T b) {
+    if (a > b) {
         a = b;
         return true;
     }
     return false;
 }
-int gcd(int a, int b){
-
+template <class T> inline bool chmax(T &a, T b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+int gcd(int a, int b) {
     if (b == 0)
         return a;
     else
         return gcd(b, a % b);
 }
-int lcm(int a, int b){
+int lcm(int a, int b) {
     return a * b / gcd(a, b);
 }
 //各桁の和
-int sumDight(int x){
+int sumDight(int x) {
     int sum = 0;
-    while (x >0) {
-        sum += (x %10);
+    while (x > 0) {
+        sum += (x % 10);
         x /= 10;
     }
     return sum;
 }
 //回文数
-int divideReverse(int x){
+int divideReverse(int x) {
     int reverse = 0;
     int r;
-    while (x >0){
+    while (x > 0) {
         r = x % 10;
         reverse = reverse * 10 + r;
         x /= 10;
     }
     return reverse;
 }
+int ksinsu(ll x, ll k) {
+    int sum = 0;
+    while (x > 0) {
+        x /= k;
+        sum++;
+    }
+    return sum ;
+}
 
-int main(){
+int main() {
     cin.tie(0);
     cout.tie(0);
     ios::sync_with_stdio(false);
 
     ll n;
-    cin >> n;
-    int mi = 10000;
-    
-    for (int i = 1; i <=n/2; i++) {
-        int a=sumDight(i)+sumDight(n-i);
-            if(a<mi){
-                mi = a;
-            }
-        
-        
-    }
-    cout << mi << endl;
+    int k;
+    cin >> n >> k;
+    cout << ksinsu(n, k) << endl;
 }
